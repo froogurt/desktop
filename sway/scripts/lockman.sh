fe419bc6018a5e115ef04c38c589f5278eefa7dc
Kaedyn <kaedynkessler@gmail.com>
2023-05-01 13:14:01 -0400

    added proper config files

---
+++ sway/scripts/lockman.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#\
+# Times the screen off and puts it to background
+swayidle \
+    timeout  600 'swaymsg "output * power off"' \
+    resume 'swaymsg "output * power on"' &
+# Locks the screen immediately
+
+swaylock
+# Kills last background task so idle timer doesn't keep running
+kill %%
